41a10e91eb9dbbfff448856509a6182c09691387
Estrella Vargas <Estrella Vargas>
2021-06-12 16:34:21 +0000

    edited settings.py

--- sample/settings.py
+++ sample/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '3@47w+4-f^m%u@$@+z#v428&stihz3aqr8(fa*m6vdfm*t+(*2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['estrellavargascode.com', 'estrellavargascode', 'localhost', 'http://68.183.113.249', '68.183.113.249', ]
 
 
 # Application definition
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'sample.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'polls',
+        'USER': 'django',
+        'PASSWORD': os.environ.get('DJANGO_DB_PASSWORD', None),
+        'HOST': 'private-django-db-do-user-9265292-0.b.db.ondigitalocean.com'
+        'PORT': 25060,
     }
 }
 
@@ -118,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
